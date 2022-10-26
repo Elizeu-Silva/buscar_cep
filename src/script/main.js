de42cdb427b4a979003298d6461a6a67dcb66219
Elizeu-Silva <elizeu.wissel@gmail.com>
2022-10-26 16:19:47 -0300

    Projeto Buscador de endereço via Cep

--- src/script/main.js
+++ src/script/main.js
@@ -5,47 +5,41 @@ const bairro = document.querySelector('.bairro');
 const endereço = document.querySelector('.endereço');
 const button = document.querySelector('.btn');
 
-
 form.addEventListener('submit', (event)=>{
   event.preventDefault();
+  
 })
 
-button.addEventListener('click', function buscarCep(){
-
-  let cep = document.querySelector('.cep').value;
-  let url = `http://viacep.com.br/ws/${cep}/json/`;
-
-    if(cep.length !== 8){
-      alert('Cep inválido')
-      return
-    }
+const buscarCep = async (cep) =>{
+  const APIcep = await fetch(`http://viacep.com.br/ws/${cep}/json/`);
   
-  
-    fetch(url).then(function(response){
-      response.json().then(function(data){
-        renderData(data)
-      })
-  });
+ if(APIcep.status === 200){
+    const data = await APIcep.json()
+    return data
+ }
 
-} )
-
-
-
-function renderData(data){
-
-  if(data){
-    estado.style.background = '#fff';
-    estado.value = data.uf;  
-
-    cidade.style.background = '#fff';
-    cidade.value = data.localidade;  
-
-    bairro.style.background = '#fff';
-    bairro.value = data.bairro;  
-
-    endereço.style.background = '#fff';
-    endereço.value = data.logradouro;  
-  }
 };
 
+const renderCep = async (cep) =>{
+  const data = await buscarCep(cep)
+  
+    if(data){
+      estado.style.background = '#fff';
+      estado.value = data.uf;  
 
+      cidade.style.background = '#fff';
+      cidade.value = data.localidade;  
+
+      bairro.style.background = '#fff';
+      bairro.value = data.bairro;  
+
+      endereço.style.background = '#fff';
+      endereço.value = data.logradouro;  
+    }
+  
+}
+
+button.addEventListener('click', ()=>{
+  let cep = document.querySelector('.cep');
+  renderCep(cep.value)
+})
